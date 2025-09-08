0d41d16d2738b4c9e0347133c65444774adfcdfa
Irwing <irwing.devbr@gmail.com>
2025-09-08 19:41:51 -0300

    fix: fix danca.py time limit exceeded error

--- OBI2025/Fase 2/danca.py
+++ OBI2025/Fase 2/danca.py
@@ -1,32 +1,22 @@
 def main():
     [line, col, steps] = map(int, input().split())
-    matriz = [[0 for _ in range(col)] for _ in range(line)]
-    index = 1
-    for i in range(line):
-        for j in range(col):
-            matriz[i][j] = index
-            index += 1
+    lines = list(range(line))
+    cols = list(range(col))
     
     for _ in range(steps):
         [position, n_str, k_str] = map(str, input().split())
-        n = int(n_str)
-        k = int(k_str)
+        n = int(n_str) - 1
+        k = int(k_str) - 1
         if position == "C":
-            for i in range(line):
-                nums = []
-                nums.append(matriz[i][n-1])
-                nums.append(matriz[i][k-1])
-                matriz[i][n-1] = nums.pop()
-                matriz[i][k-1] = nums.pop()
+            cols[n], cols[k] = cols[k], cols[n]
         elif position == "L":
-            for i in range(col):
-                nums = []
-                nums.append(matriz[n-1][i])
-                nums.append(matriz[k-1][i])
-                matriz[n-1][i] = nums.pop()
-                matriz[k-1][i] = nums.pop()
+            lines[n], lines[k] = lines[k], lines[n]
 
-    for l in matriz:
+    for i in range(line):
+        l = []
+        for j in range(col):
+            elements = lines[i] * col + cols[j] + 1
+            l.append(elements)
         print(*l)
 
 if __name__ == "__main__":
